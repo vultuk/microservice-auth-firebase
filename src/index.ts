99781eda45b2aebab6dcd2b7bddfec8a431bcbb4
Simon Skinner <simon@vult.uk>
2021-04-30 17:11:09 +0100

    Initial Loadout

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import {NextFunction, Request, Response} from 'express';
 
-import {Admin, Auth as AuthImport, AuthAdmin, AuthAdministration} from './deps';
+import {Admin, Auth as AuthImport, AuthAdmin, AuthAdministration as AuthAdmins} from './deps';
 import {Settings} from './Types/Settings';
 
 export type Auth = AuthImport;
+export type AuthAdministration = AuthAdmins;
 export * from './Types';
 
 declare global {
   namespace Express {
     interface Request {
       auth: Auth;
-      authAdmin: AuthAdministration;
+      authAdmin: AuthAdmins;
     }
   }
 }
