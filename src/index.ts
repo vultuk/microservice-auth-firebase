56963c76064f6c745ff00e16e9fb317f65293ada
Simon Skinner <simon@vult.uk>
2021-04-30 16:02:40 +0100

    Initial Loadout

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { NextFunction, Request, Response } from 'express';
+import {NextFunction, Request, Response} from 'express';
 
-import { Admin, Auth as AuthImport, AuthAdmin, AuthAdministration } from './deps';
-import { Settings } from './Types/Settings';
+import {Admin, Auth as AuthImport, AuthAdmin, AuthAdministration} from './deps';
+import {Settings} from './Types/Settings';
 
 export type Auth = AuthImport;
 export * from './Types';
@@ -16,7 +16,8 @@ declare global {
 }
 
 export default (settings: Settings) => (req: Request, res: Response, next: NextFunction) => {
-  Admin.initializeApp(settings);
+  // Admin.initializeApp(settings);
+  AuthAdmin.initializeApp(settings);
   req.auth = Admin.auth();
   req.authAdmin = AuthAdmin.auth();
 
