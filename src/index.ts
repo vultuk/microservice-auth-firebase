b2d3ff3440e7460b4ea99d4df0dfa5889b0abcec
Simon Skinner <simon@vult.uk>
2021-04-30 16:02:43 +0100

    0.0.7

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import {NextFunction, Request, Response} from 'express';
+import { NextFunction, Request, Response } from 'express';
 
-import {Admin, Auth as AuthImport, AuthAdmin, AuthAdministration} from './deps';
-import {Settings} from './Types/Settings';
+import { Admin, Auth as AuthImport, AuthAdmin, AuthAdministration } from './deps';
+import { Settings } from './Types/Settings';
 
 export type Auth = AuthImport;
 export * from './Types';
